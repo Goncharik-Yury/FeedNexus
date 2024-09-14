c696e7ff596d3cbf27f529c4a344494567d51dc3
Yury Goncharik <selest.gyv@gmail.com>
2024-09-14 08:09:03 +0300

    Add basic RSS controller

---
+++ FeedNexus/FeedNexus/Controllers/RssFeedController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using FeedNexus.Models;
+using FeedNexus.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeedNexus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RssFeedController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RssFeedController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRssFeed([FromBody] RssFeed rssFeed)
+        {
+            if (rssFeed == null || string.IsNullOrEmpty(rssFeed.Url))
+                return BadRequest("Invalid data.");
+
+            rssFeed.CreatedAt = DateTime.UtcNow;
+
+            _context.RssFeeds.Add(rssFeed);
+            await _context.SaveChangesAsync();
+
+            return Ok(rssFeed);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRssFeeds()
+        {
+            var rssFeeds = await _context.RssFeeds.ToListAsync();
+            return Ok(rssFeeds);
+        }
+    }
+}
